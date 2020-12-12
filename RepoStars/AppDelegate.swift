1481d715e9b1471f6b55debe68fb05a10320a029
Erik Nascimento <erik@2eg.com.br>
2020-12-12 18:54:58 -0300

    initial commit

---
+++ RepoStars/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  RepoStars
+//
+//  Created by Erik Nascimento on 12/12/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
